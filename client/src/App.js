30f411a4a22b59b120b7f21e8f2a7feb3e57312e
athirasnair1998 <athirasnair198@gmail.com>
2025-06-24 10:01:05 +0530

    Fix: user role default handling and login response improvements

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Register from './pages/Register';
+import Login from './pages/Login'; // 🔹 import login
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/register" />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} /> {/* 🔹 add this */}
       </Routes>
     </Router>
   );
